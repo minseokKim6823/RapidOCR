337aad9adb238d7a53cb83e74d8f1ca441372096
김민석 <minseokkim6823@taehwainno.com>
2025-04-01 09:59:55 +0900

    Detect Once

--- OcrLib/OcrLite.cs
+++ OcrLib/OcrLite.cs
@@ -11,15 +11,16 @@ namespace OcrLiteLib
     {
         public bool isPartImg { get; set; }
         public bool isDebugImg { get; set; }
+
         private DbNet dbNet;
-        //private AngleNet angleNet;
-        private CrnnNet crnnNet;
+        private CrnnNet crnnNet1;
+        private CrnnNet crnnNet2;
 
         public OcrLite()
         {
             dbNet = new DbNet();
-            //angleNet = new AngleNet();
-            crnnNet = new CrnnNet();
+            crnnNet1 = new CrnnNet();
+            crnnNet2 = new CrnnNet();
         }
 
         public void InitModels(string detPath, string rec1Path, string rec2Path, string keys1Path, string keys2Path, int numThread)
@@ -27,8 +28,8 @@ namespace OcrLiteLib
             try
             {
                 dbNet.InitModel(detPath, numThread);
-                crnnNet.InitModel(rec1Path, keys1Path, numThread);
-                crnnNet.InitModel(rec2Path, keys2Path, numThread);
+                crnnNet1.InitModel(rec1Path, keys1Path, numThread);
+                crnnNet2.InitModel(rec2Path, keys2Path, numThread);
             }
             catch (Exception ex)
             {
@@ -43,29 +44,23 @@ namespace OcrLiteLib
             Mat originSrc = CvInvoke.Imread(img, ImreadModes.Color);//default : BGR
             int originMaxSide = Math.Max(originSrc.Cols, originSrc.Rows);
 
-            int resize;
-            if (maxSideLen <= 0 || maxSideLen > originMaxSide)
-            {
-                resize = originMaxSide;
-            }
-            else
-            {
-                resize = maxSideLen;
-            }
+            int resize = (maxSideLen <= 0 || maxSideLen > originMaxSide) ? originMaxSide : maxSideLen;
             resize += 2 * padding;
+
             Rectangle paddingRect = new Rectangle(padding, padding, originSrc.Cols, originSrc.Rows);
             Mat paddingSrc = OcrUtils.MakePadding(originSrc, padding);
 
             ScaleParam scale = ScaleParam.GetScaleParam(paddingSrc, resize);
 
-            return DetectOnce(paddingSrc, paddingRect, scale, boxScoreThresh, boxThresh, unClipRatio, doAngle, mostAngle);
+            return DetectOnce(paddingSrc, paddingRect, scale, boxScoreThresh, boxThresh, unClipRatio);
         }
 
         private OcrResult DetectOnce(Mat src, Rectangle originRect, ScaleParam scale, float boxScoreThresh, float boxThresh,
-                      float unClipRatio, bool doAngle, bool mostAngle)
+                      float unClipRatio)
         {
             Mat textBoxPaddingImg = src.Clone();
             int thickness = OcrUtils.GetThickness(src);
+
             Console.WriteLine("=====Start detect=====");
 
             Stopwatch totalWatch = new Stopwatch();
@@ -117,7 +112,6 @@ namespace OcrLiteLib
                 }
 
                 Rectangle boxRect = OcrUtils.GetBoundingBox(box.Points);
-
                 if (boxRect.Width <= 0 || boxRect.Height <= 0)
                 {
                     Console.WriteLine("Skipping box: invalid bounding box.");
@@ -148,23 +142,22 @@ namespace OcrLiteLib
                 }
             }
 
-            //List<Angle> angles = angleNet.GetAngles(partImages, doAngle, mostAngle);
+            List<TextLine> textLines = new List<TextLine>();
 
-            // 180도 회전 및 디버그 이미지 출력
-            //for (int i = 0; i < partImages.Count; ++i)
-            //{
-            //    if (angles[i].Index == 1)
-            //    {
-            //        partImages[i] = OcrUtils.MatRotateClockWise180(partImages[i]);
-            //    }
+            Rectangle micrRoi = roiRects[2];
 
-            //    if (isDebugImg)
-            //    {
-            //        CvInvoke.Imshow($"DebugImg({i})", partImages[i]);
-            //    }
-            //}
+            for (int i = 0; i < partImages.Count; i++)
+            {
+                Rectangle boxRect = OcrUtils.GetBoundingBox(filteredTextBoxes[i].Points);
+                bool isMicrBox = micrRoi.IntersectsWith(boxRect);
 
-            List<TextLine> textLines = crnnNet.GetTextLines(partImages);
+                var textLine = isMicrBox
+                    ? crnnNet2.GetTextLine(partImages[i])  // MICR 영역
+                    : crnnNet1.GetTextLine(partImages[i]); // 일반 영역
+
+
+                textLines.Add(textLine);
+            }
 
             // 최종 결과 조합
             List<TextBlock> textBlocks = new List<TextBlock>();
@@ -173,15 +166,12 @@ namespace OcrLiteLib
             {
                 TextBlock textBlock = new TextBlock
                 {
+
                     BoxPoints = filteredTextBoxes[i].Points,
                     BoxScore = filteredTextBoxes[i].Score,
-                    //AngleIndex = angles[i].Index,
-                    //AngleScore = angles[i].Score,
-                    //AngleTime = angles[i].Time,
                     Text = textLines[i].Text,
                     CharScores = textLines[i].CharScores,
                     CrnnTime = textLines[i].Time,
-                    //BlockTime = angles[i].Time + textLines[i].Time
                 };
 
                 textBlocks.Add(textBlock);
@@ -195,11 +185,8 @@ namespace OcrLiteLib
 
             Console.WriteLine($"===== 전체 인식 시간 : {totalTimeMs} ms =====");
 
-
-
-
             // 결과 반환
-            OcrResult ocrResult = new OcrResult
+            return new OcrResult
             {
                 TextBlocks = textBlocks,
                 DbNetTime = dbNetTime,
@@ -207,12 +194,16 @@ namespace OcrLiteLib
                 StrRes = string.Join(Environment.NewLine, textBlocks.ConvertAll(tb => tb.Text)),
                 DetectTime = totalTimeMs
             };
-            Console.WriteLine(ocrResult);
-            return ocrResult;
         }
 
     }
 }
+
+
+
+
+
+
 
 //using Emgu.CV;
 //using Emgu.CV.CvEnum;
@@ -220,8 +211,7 @@ namespace OcrLiteLib
 //using System.Collections.Generic;
 //using System.Drawing;
 //using System.Diagnostics;
-//using System.Reflection;
-//using System.Text;
+
 
 //namespace OcrLiteLib
 //{
@@ -230,23 +220,22 @@ namespace OcrLiteLib
 //        public bool isPartImg { get; set; }
 //        public bool isDebugImg { get; set; }
 //        private DbNet dbNet;
-//        private AngleNet angleNet;
-//        private CrnnNet crnnNet;
+//        private CrnnNet crnnNet1;
+//        private CrnnNet crnnNet1;
 
 //        public OcrLite()
 //        {
 //            dbNet = new DbNet();
-//            angleNet = new AngleNet();
 //            crnnNet = new CrnnNet();
 //        }
 
-//        public void InitModels(string detPath, string clsPath, string recPath, string keysPath, int numThread)
+//        public void InitModels(string detPath, string rec1Path, string rec2Path, string keys1Path, string keys2Path, int numThread)
 //        {
 //            try
 //            {
 //                dbNet.InitModel(detPath, numThread);
-//                angleNet.InitModel(clsPath, numThread);
-//                crnnNet.InitModel(recPath, keysPath, numThread);
+//                crnnNet.InitModel(rec1Path, keys1Path, numThread);
+//                crnnNet.InitModel(rec2Path, keys2Path, numThread);
 //            }
 //            catch (Exception ex)
 //            {
@@ -347,43 +336,24 @@ namespace OcrLiteLib
 //                }
 //            }
 
-//            List<Angle> angles = angleNet.GetAngles(partImages, doAngle, mostAngle);
 
-//            // 180도 회전 및 디버그 이미지 출력
-//            for (int i = 0; i < partImages.Count; ++i)
+//            List<TextLine> textLines = new List<TextLine>();
+
+//            for (int i = 0; i < partImages.Count; i++)
 //            {
-//                if (angles[i].Index == 1)
+//                Rectangle boxRect = OcrUtils.GetBoundingBox(filteredTextBoxes[i].Points);
+//                float centerY = boxRect.Top + boxRect.Height / 2.0f;
+
+//                float roiThresholdY = src.Height * 0.8f;
+
+//                if (centerY >= roiThresholdY)
 //                {
-//                    partImages[i] = OcrUtils.MatRotateClockWise180(partImages[i]);
+//                    textLines.Add(crnnNet2.GetTextLine(partImages[i])); // 아래쪽은 모델2
 //                }
-
-//                if (isDebugImg)
+//                else
 //                {
-//                    CvInvoke.Imshow($"DebugImg({i})", partImages[i]);
+//                    textLines.Add(crnnNet1.GetTextLine(partImages[i])); // 나머지는 모델1
 //                }
-//            }
-
-//            List<TextLine> textLines = crnnNet.GetTextLines(partImages);
-
-//            // 최종 결과 조합
-//            List<TextBlock> textBlocks = new List<TextBlock>();
-
-//            for (int i = 0; i < textLines.Count; ++i)
-//            {
-//                TextBlock textBlock = new TextBlock
-//                {
-//                    BoxPoints = filteredTextBoxes[i].Points,
-//                    BoxScore = filteredTextBoxes[i].Score,
-//                    AngleIndex = angles[i].Index,
-//                    AngleScore = angles[i].Score,
-//                    AngleTime = angles[i].Time,
-//                    Text = textLines[i].Text,
-//                    CharScores = textLines[i].CharScores,
-//                    CrnnTime = textLines[i].Time,
-//                    BlockTime = angles[i].Time + textLines[i].Time
-//                };
-
-//                textBlocks.Add(textBlock);
 //            }
 
 //            // 박스 시각화도 ROI 필터링된 것만 그린다!
