864133e8314b69abb8fc64abd782669146f16960
김민석 <minseokkim6823@taehwainno.com>
2025-04-03 15:18:47 +0900

    ThDraftOCR,SimpleConfig 추가

---
+++ OcrLib/SimpleConfig.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+
+namespace OcrLib
+{
+    class SimpleConfig
+    {
+        private Dictionary<string, string> config = new Dictionary<string, string>();
+
+        public SimpleConfig(string path)
+        {
+            foreach (var line in File.ReadAllLines(path))
+            {
+                if (string.IsNullOrWhiteSpace(line) || line.TrimStart().StartsWith("#")) continue;
+
+                var parts = line.Split('=');
+                if (parts.Length == 2)
+                {
+                    config[parts[0].Trim()] = parts[1].Trim();
+                }
+            }
+        }
+
+        public string Get(string key) => config.ContainsKey(key) ? config[key] : null;
+        public int GetInt(string key) => int.Parse(Get(key));
+        public float GetFloat(string key) => float.Parse(Get(key));
+        public List<(float, float, float, float)> GetRoiList()
+        {
+            var rois = new List<(float, float, float, float)>();
+            foreach (var kv in config)
+            {
+                if (kv.Key.EndsWith("roi"))
+                {
+                    var parts = kv.Value.Split(',');
+                    if (parts.Length == 4 &&
+                        float.TryParse(parts[0], out float x) &&
+                        float.TryParse(parts[1], out float y) &&
+                        float.TryParse(parts[2], out float w) &&
+                        float.TryParse(parts[3], out float h))
+                    {
+                        rois.Add((x, y, w, h));
+                    }
+                }
+            }
+            return rois;
+        }
+    }
+}
---
+++ OcrLib/ThDraftOCR.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections.Generic;
+using Emgu.CV;
+using OcrLiteLib;
+
+namespace OcrLib
+{
+    public class ThDraftOCR
+    {
+        private OcrLite ocr;
+        private SimpleConfig config;
+        private string imgPath;
+        private int maxSideLen;
+        private Mat image;
+        private List<(float x, float y, float width, float height)> roiRatios;
+            
+        public ThDraftOCR()
+        {
+            ocr = new OcrLite();
+            config = new SimpleConfig("config.ini");
+
+            maxSideLen = config.GetInt("maxSideLen");
+            int numThread = config.GetInt("numThread");
+
+            string detPath = config.Get("detPath");
+            string rec1Path = config.Get("num_rec_path");
+            string rec2Path = config.Get("micr_rec_path");
+            string keys1Path = config.Get("num_keys_path");
+            string keys2Path = config.Get("micr_keys_path");
+            roiRatios = config.GetRoiList();
+            image = CvInvoke.Imread(imgPath, Emgu.CV.CvEnum.ImreadModes.Color);
+            ocr.InitModels(detPath, rec1Path, rec2Path, keys1Path, keys2Path, numThread);
+        }
+        public OcrResult DetectFilter(object imgPath)
+        {
+
+            var result = ocr.DetectFilter(
+                img: imgPath,
+                padding: 0,
+                maxSideLen: maxSideLen,
+                boxScoreThresh: 0.5f,
+                boxThresh: 0.3f,
+                unClipRatio: 1.6f,
+                roiRatios: roiRatios
+                );
+            //MessageBox.Show(result.ToString());
+            return result;
+        }
+
+        public OcrResult DetectCrop(object imgPath)
+        {
+
+            return ocr.DetectCrop(
+                img: imgPath,
+                padding: 0,
+                maxSideLen: maxSideLen,
+                boxScoreThresh: 0.5f,
+                boxThresh: 0.3f,
+                unClipRatio: 1.6f,
+                roiRatios: roiRatios
+                );
+        }
+
+        public OcrResult DetectMask(object imgPath)
+        {
+
+            return ocr.DetectMask(
+                img: imgPath,
+                padding: 0,
+                maxSideLen: maxSideLen,
+                boxScoreThresh: 0.5f,
+                boxThresh: 0.3f,
+                unClipRatio: 1.6f,
+                roiRatios: roiRatios
+                );
+        }
+
+        public OcrResult DetectNumber(object imgPath)
+        {
+
+            return ocr.DetectNumber(
+                img: imgPath,
+                padding: 0,
+                maxSideLen: maxSideLen,
+                boxScoreThresh: 0.5f,
+                boxThresh: 0.3f,
+                unClipRatio: 1.6f,
+                roiRatios: roiRatios
+                );
+        }
+        public OcrResult DetectMICR(object imgPath)
+        {
+
+            return ocr.DetectMICR(
+                img: imgPath,
+                padding: 0,
+                maxSideLen: maxSideLen,
+                boxScoreThresh: 0.5f,
+                boxThresh: 0.3f,
+                unClipRatio: 1.6f,
+                roiRatios: roiRatios
+                );
+        }
+    }
+    
+}
